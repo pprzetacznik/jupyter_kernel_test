24e952ad6df5ca8ceaa70fb6ef1cbf288d2afe27
Piotr Przetacznik <piotr.przetacznik@gmail.com>
2015-07-26 00:23:51 +0200

    Prepare test_ipython.py for IElixir

--- test_ipython.py
+++ test_ipython.py
@@ -4,23 +4,31 @@ import unittest
 import jupyter_kernel_test as jkt
 
 class IRkernelTests(jkt.KernelTests):
-    kernel_name = "python3"
+    kernel_name = "ielixir"
 
-    language_name = "python"
+    language_name = "elixir"
 
-    code_hello_world = "print('hello, world')"
+    code_hello_world = "IO.puts(\"hello, world\")"
 
     completion_samples = [
         {
-            'text': 'zi',
-            'matches': {'zip'},
+          'text': 'En',
+          'matches': {'um'},
         },
+        {
+          'text': 'Enu',
+          'matches': {'m'},
+        },
+        {
+          'text': 'Enum',
+          'matches': {'Enum', 'Enumerable'},
+        }
     ]
 
-    complete_code_samples = ['1', "print('hello, world')", "def f(x):\n  return x*2\n\n"]
-    incomplete_code_samples = ["print('''hello", "def f(x):\n  x*2"]
+    complete_code_samples = ["1", "IO.puts(\"abc\")", "fun = fn x -> x*2 end"]
+    incomplete_code_samples = ["case x do", "fun = fn x -> x*2"]
+    invalid_code_samples = ["asdf", "case x of", "1aaa"]
 
-    code_page_something = "zip?"
 
 if __name__ == '__main__':
     unittest.main()
